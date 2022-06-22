56ad4646be0b11b61ea09ac76f58066b9d06f8f6
Kamaran Ahmed <ahmedkamaran893@gmail.com>
2022-06-22 23:04:49 +0530

    PHASE1FINAL

--- src/Controllers/blogcontroller.js
+++ src/Controllers/blogcontroller.js
@@ -2,16 +2,54 @@ const blogModel = require("../Models/blogModel")
 const moment = require('moment')
 const lodash = require('lodash')
 const mongoose = require("mongoose")
+const authorModel = require('../Models/authorModel')
 
 
 
 /*####################################################### POST API ####################################################*/
 
+
+
+
 const createBlogDoc = async function (req, res) {
     try {
         let blogData = req.body
-        
+          console.log(blogData)
+        if (Object.keys(blogData).length !== 0) {   
+            let authorId= blogData.authorId
+            let authorDetails= await authorModel.findById(authorId)
+            console.log(authorDetails)
+            if(!authorDetails) return res.status(404).send({msg:"No author exist with this authorId"})
+            let savedblogData = await blogModel.create(blogData)
+            res.status(201).send({ msg: savedblogData })
+        }
+         else {res.status(400).send({ msg: "BAD REQUEST" })}
+  
+}
+    catch (err) {
+        console.log("This is the error :", err.message)
+        res.status(500).send({ msg: "Error", error: err.message })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+const createBlogDoc1 = async function (req, res) {
+    try {
+        let blogData = req.body
         if (Object.keys(blogData).length != 0) {
+            let authorId = blogData.authorId;
+            auther = await authorModel.findById(authorId)
+        
             let savedblogData = await blogModel.create(blogData)
             res.status(201).send({ msg: savedblogData })
         }
@@ -30,15 +68,16 @@ const blogs = async (req, res) => {
 
     try {
     req.query["isDeleted"] = false
-    req.query["isPublished"] = true
+    req.query["isPublished"] = true 
+    if(req.query.tags)
+    req.query.tags = {"$all":req.query.tags.split(",")}
+    if(req.query.subCategory)
+    req.query.subCategory = {"$all":req.query.subCategory.split(",")}
     let blogs = await blogModel.find(req.query)
-
-    if (Object.keys(blogs).length === 0) {
-        return res.status(404).send({ status: false, msg: "Data not Found" })
-    }
-
+    if (Object.keys(blogs).length === 0) return res.status(404).send({ status: false, msg: "Data not Found" })
     return res.status(200).send({ status: true, data: blogs })
-    } catch (error) {
+    }
+    catch (error) {
         res.status(500).send({ status: false, msg: error.message })
     }
 
@@ -53,6 +92,10 @@ const blogs = async (req, res) => {
 /*##################################################### PUT API ##################################################*/
 
 
+
+
+
+
 const blogPut = async (req, res) => {
 
 
@@ -61,7 +104,7 @@ const blogPut = async (req, res) => {
         if (Object.keys(blog).length===0) return res.status(400).send({ status: false, msg: "Bad Request" });
         let blogId = req.params.blogId;
         let blogToBeUpdted = await blogModel.findOne({ _id: blogId, isDeleted: false })
-        if (Object.keys(blogToBeUpdted).length===0) return res.status(404).send({ status: false, msg: "Blog DoesNot Exist" });
+        if (Object.keys(blogToBeUpdted).length===0) return res.status(404).send({ status: false, msg: "Blog does not exist" });
         blog["tags"] = lodash.uniq(req.body.tags.concat(blogToBeUpdted.tags));
         blog["subCategory"] = lodash.uniq(req.body.subCategory.concat(blogToBeUpdted.subCategory));
         blog["isPublished"] = true
@@ -69,8 +112,8 @@ const blogPut = async (req, res) => {
 
         let blogUpdated = await blogModel.findOneAndUpdate({ _id: blogId }, blog, { new: true, upsert: true, strict: false })
 
-        if (!blogUpdated) {
-            return res.status(404).send({ status: false, msg: "Use Not Exist" })
+        if (Object.keys(blogUpdated).length===0) {
+            return res.status(404).send({ status: false, msg: "Blog does not exist" })
         }
 
         return res.status(201).send({ status: true, data: blogUpdated })
@@ -82,16 +125,56 @@ const blogPut = async (req, res) => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const blogPut1 = async (req, res) => {
+
+
+    try {
+        let blog = req.body;
+        if (Object.keys(blog).length===0) return res.status(400).send({ status: false, msg: "Bad Request" });
+        let blogToBeUpdted = await blogModel.findOne({ _id: req.params.blogId, isDeleted: false })
+        if (Object.keys(blogToBeUpdted).length===0) return res.status(404).send({ status: false, msg: "Blog DoesNot Exist" });
+        blog.isPublished = true
+        blog.publishedAt = moment().format("YYYY MM DDThh:mm:ss.SSS[Z]");
+        blog.$addToSet = {subCategory: {"$each":blog.subCategory},tags: {"$each":blog.tags}}
+        delete blog.subCategory
+        delete blog.tags
+        let blogUpdated = await blogModel.findOneAndUpdate({ _id: req.params.blogId }, blog, { new: true, upsert: true, strict: false })
+        if (!blogUpdated) return res.status(404).send({ status: false, msg: "Use Not Exist" })
+        return res.status(201).send({ status: true, data: blogUpdated })
+        } catch (error) {
+                res.status(500).send({ status: false, msg: error.message })
+          }
+
+}
+
+
+// find({},{},{})
+
+
 /*############################################# DELETE APIS ################################################*/
 
 
 const blogDeletById = async (req, res) => {
     try {
-    let blogId = req.params.blogId;
-    if (!blogId) return res.status(400).send({ status: false, msg: "Bad Request" });
-    let blogToBeDeleted = await blogModel.findOne({ _id: blogId, isDeleted: false })
+    if (!req.params.blogId) return res.status(400).send({ status: false, msg: "Bad Request" });
+    let blogToBeDeleted = await blogModel.findOne({ _id: req.params.blogId, isDeleted: false })
     if (!blogToBeDeleted) return res.status(404).send({ status: false, msg: "Blog DoesNot Exist" });
-    await blogModel.findOneAndUpdate({ _id: blogId }, { isDeleted: true })
+    await blogModel.findOneAndUpdate({ _id: req.params.blogId }, { isDeleted: true })
     res.status(200).send()
     } catch (error) {
         res.status(500).send({ status: false, msg: error.message })
--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,24 +1,133 @@
 
-
-const validateAuthorId = (req,res,next)=>{
-    var regex = /[a-f0-9]{24}/;
-    if(!regex.test(req.body.authorId)) return res.status(400).send({status:false,msg:"Author Id is Not Valid"})
-    next()
+const tags = []
+const category = []
+const subCategory = []
+function minimumCost(a, b)
+{
+    // Stores the frequency of string
+    // a and b respectively
+    var fre1 = Array(256).fill(0), fre2= Array(256).fill(0);
+ 
+    // Store the frequencies of
+    // characters in a
+    a.split('').forEach(c => {
+        fre1[c.charCodeAt(0)]++;
+    });
+ 
+    // Store the frequencies of
+    // characters in b
+    b.split('').forEach(c => {
+        fre2[c.charCodeAt(0)]++;
+    });
+ 
+    // Minimum cost to convert A to B
+    var mincost = 0;
+ 
+    // Find the minimum cost
+    for (var i = 0; i < 256; i++) {
+        mincost += Math.abs(fre1[i]
+                       - fre2[i]);
+    }
+ 
+    // Print the minimum cost
+    return mincost ;
 }
 
 
 
 
 
-const validateEmail = (req,res,next)=>{
+
+const validateTags = (tags)=>{
+  if(!Array.isArray(tags)){
+      return tags.split(",")
+  }
+  return tags
+
+}
+
+
+const validateSubCategory = (subCategory)=>{
+    if(!Array.isArray(subCategory)){
+        return subCategory.split(",")
+    }
+    return subCategory
+}
+
+const validateCategory = (category)=>{
+
+}
+
+const validatePassword = (password)=>{
+
+}
+
+const validateName = (Name,res,whatis)=>{
+      let regex = /[A-Z]{1}[a-z]{2}[a-z]*/
+      if(!regex.test(Name)) return res.status(400).send({status:false,msg:"Name is Not Valid"})
+
+
+}
+
+
+
+
+
+
+
+
+const validateObjectId = (id,res,whatIs)=>{
+    var regex = /[a-f0-9]{24}/;
+    if(!regex.test(id)) return res.status(400).send({status:false,msg:`${whatIs} is Not Valid`})
+    
+}
+
+
+
+
+
+const validateEmail = (email,res)=>{
     var regex = /\S+@\S+\.\S+/;
-    if(!regex.test(req.body.email)) return res.status(400).send({status:false,msg:"Email is Not Valid"})
-    next()
+    if(!regex.test(email)) return res.status(400).send({status:false,msg:"Email is Not Valid"})
 
 
   }
+ 
+
+const validateRequest = (req,res,next)=>{
+    if(req.query){
+       if(req.query.authoeId)
+          validateObjectId(req.authoeId,res,"Author Id")
+       if(req.query.blogId)
+          validateObjectId(req.blogId,res,"BlogId Id")
+       if(req.query.tags)
+          req.query.tags = validateTags(req.query.tags)
+       if(req.query.subCategory)
+          req.query.subCategory = validateTags(req.query.subCategory)
+    }
+
+    if(req.param){
+        if(req.param.authoeId)
+           validateObjectId(req.authoeId,res,"Author Id")
+        if(req.param.blogId)
+           validateObjectId(req.blogId,res,"BlogId Id")
+    }
+
+    if(req.body){
+        if(req.body.authoeId)
+           validateObjectId(req.body.authoeId);
+        if(req.body.tags)
+           validateTags(req.body.tags);
+        if(req.body.sebCategory)
+           validateSubCategory(req.body.sebCategory);
+        if(req.body.fname);
+           validateName(req.body.fname,res,"help")
+        if(req.body.lname);
+           validateName(req.body.lname,res,"help")
+           
+    }
+
+    next()
+}
 
 
-
-  module.exports.validateAuthorId =validateAuthorId
-  module.exports.validateEmail = validateEmail
